b3f978f2e7708847b82ec6414498f9b554ea3f63
mk293822 <mkt293822@gamil.com>
2026-01-18 10:07:08 +0630

    feat: delete the payments to change the folder name

--- apps/Payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('stripe/checkout', views.checkout, name='stripe_checkout'),
-    path('stripe/success', views.stripe_success, name='stripe_success'),
-    path('stripe/failure', views.stripe_failure, name='stripe_failure'),
-]
